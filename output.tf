ea3b1fceec9aa419503d6bc2d7b9a328d534ae5c
angryluke <lbolli@hashicorp.com>
2021-07-29 08:53:24 +0200

    add * to each output variable due to added count set to resource creation

--- output.tf
+++ output.tf
@@ -1,18 +1,18 @@
 output "rds_hostname" {
   description = "RDS instance hostname"
-  value       = aws_db_instance.demo.address
+  value       = aws_db_instance.demo.*.address
   sensitive   = true
 }
 
 output "rds_port" {
   description = "RDS instance port"
-  value       = aws_db_instance.demo.port
+  value       = aws_db_instance.demo.*.port
   sensitive   = true
 }
 
 output "rds_username" {
   description = "RDS instance root username"
-  value       = aws_db_instance.demo.username
+  value       = aws_db_instance.demo.*.username
   sensitive   = true
 }
 
